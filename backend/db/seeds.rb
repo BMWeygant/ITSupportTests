5a6026fba843984404e73bb1bd995e2ce9a2a7be
BMWeygant <bobbydigital108@gmail.com>
2020-04-02 19:19:30 -0400

    Seeded db

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.new(query:"IT stands for what?", correct_answer:"Information Technology", answer:"")
+Question.new(query:"Who was the First Avenger?", correct_answer:"Captain America", answer:"")
+Question.new(query:"What is Superman's gf name?", correct_answer:"Lois Lane", answer:"")
+Question.new(query:"Best Wu-Tang Clan member?", correct_answer:"Method Man", answer:"")
+Question.new(query:"Rick James' preffered diet?", correct_answer:"Cocaine", answer:"")
