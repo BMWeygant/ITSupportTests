3e54eb5339060ea555a6942e9b2e5d3767f3194a
BMWeygant <bobbydigital108@gmail.com>
2020-04-02 16:37:25 -0400

    Added questions controller

---
+++ backend/app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+    def index
+        questions = Question.all
+        render json: questions
+      end
+  
+      def create
+        question = Question.new(question_params)
+        render json: question
+      end
+    
+      def show
+        question = Question.find_by(id: params[:id])
+        render json: question
+      end
+    
+      private
+      def question_params
+        params.require(:question).permit(:query, :correct_answer, :answer)
+      end
+end
