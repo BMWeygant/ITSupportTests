852dbccfafbb5bd90bf6572d7e8d78213e83f9bb
BMWeygant <bobbydigital108@gmail.com>
2020-04-24 10:21:44 -0400

    Installed Popper and react-bootstrap

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,7 +8,15 @@ export default class Home extends Component {
       return(
         <div className="home-page">
           <h1>Welcome</h1>
-          <Link to = "/questions" className='start-link' activeclassname="activeLink">Start</Link>
+          <h3>Please Select Which Course You Wish To Practice</h3>
+          <Link to = "/fundamentals" className='start-link' activeclassname="activeLink">Technical Fundamentals</Link>
+          <Link to = "/networking" className='start-link' activeclassname="activeLink">Networking</Link>
+          <Link to = "/operating-systems" className='start-link' activeclassname="activeLink">Operating Systems</Link>
+          <DropdownButton id="basic-dropdown-button" title="Dropdown Button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton>
         </div>
 
       )
